6033e14921c566503404c50f696301aa7f3661ae
Max De Marzi <maxdemarzi@hotmail.com>
2012-10-09 11:07:29 -0500

    import working now

--- neo_holdings.rb
+++ neo_holdings.rb
@@ -42,3 +42,40 @@ def scrape_holdings
     end
   end
 end
+
+def create_graph
+  neo = Neography::Rest.new
+  letters = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+  letters.each do |letter|
+    puts "Creating Graph for letter #{letter} funds..."  
+    funds = JSON.load(File.open("data/funds/#{letter}_funds.json", 'r').read)
+    funds.each_pair do |symbol, name|
+      commands = []
+      commands << [:create_node, {:name => name, :symbol => symbol}]
+      commands << [:add_node_to_index, "funds_index", "name", name, "{0}"]
+      commands << [:add_node_to_index, "funds_index", "symbol", symbol, "{0}"]
+
+      batch_results = neo.batch *commands
+      fund_node_id = batch_results.first["body"]["self"].split('/').last
+      
+      holdings = JSON.load(File.open("data/holdings/#{symbol}_holdings.json", 'r').read)
+      commands = []
+      holdings.each do |holding|
+        name = holding["company"] || ""
+        symbol = holding["symbol"] || ""
+        ytd_return = holding["return"] || ""
+        commands << [:create_unique_node, "holdings", "name", name, {:name => name, :symbol => symbol, :ytd_return => ytd_return}]  
+      end      
+      batch_results = neo.batch *commands
+      commands = []
+            
+      batch_results.each_with_index do |result, index|
+        holding_node_id = result["body"]["self"].split('/').last
+        percent = holdings[index]["percent"] || ""
+        commands << [:create_relationship, "holds", fund_node_id, holding_node_id, {:percent => percent}] 
+        #puts "Fund Node: #{fund_node_id} Holding Node: #{holding_node_id} Percent #{percent}"
+      end      
+      batch_results = neo.batch *commands
+    end
+  end
+end
